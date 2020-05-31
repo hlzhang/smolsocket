d23a20be5afde952f75543ee26bdcb6f7a201ba2
hlzhang <hlzhang@users.noreply.github.com>
2020-05-31 23:40:04 +1200

    ci: fix code style

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,15 @@
-#[macro_use]
+#[cfg_attr(test, macro_use)]
 extern crate log;
 
 use core::fmt;
 
 use byteorder::{BigEndian, ByteOrder};
-#[cfg(all(feature = "proto-ipv4", feature = "proto-ipv6"))]
-use smoltcp::wire::{IpAddress, IpEndpoint};
 #[cfg(all(feature = "proto-ipv4"))]
 use smoltcp::wire::Ipv4Address;
 #[cfg(all(feature = "proto-ipv6"))]
 use smoltcp::wire::Ipv6Address;
+#[cfg(all(feature = "proto-ipv4", feature = "proto-ipv6"))]
+use smoltcp::wire::{IpAddress, IpEndpoint};
 
 #[cfg(all(feature = "proto-ipv4"))]
 pub use crate::ipv4::{ipv4_addr, ipv4_addr_from_bytes, SocketAddrV4};
@@ -45,7 +45,7 @@ pub fn port_from_bytes(high: u8, low: u8) -> u16 {
 pub fn port_to_bytes(port: u16) -> [u8; 2] {
     let mut buf = [0; 2];
     BigEndian::write_u16(&mut buf, port);
-    return buf;
+    buf
 }
 
 #[cfg(all(feature = "proto-ipv4", feature = "proto-ipv6"))]
@@ -80,10 +80,20 @@ impl SocketAddr {
         SocketAddr::V6(SocketAddrV6::new(ip, port))
     }
 
-    pub fn new_ip6_port(a0: u16, a1: u16, a2: u16, a3: u16,
-                        a4: u16, a5: u16, a6: u16, a7: u16,
-                        port: u16) -> Self {
-        SocketAddr::V6(SocketAddrV6::new_ip6_port(a0, a1, a2, a3, a4, a5, a6, a7, port))
+    pub fn new_ip6_port(
+        a0: u16,
+        a1: u16,
+        a2: u16,
+        a3: u16,
+        a4: u16,
+        a5: u16,
+        a6: u16,
+        a7: u16,
+        port: u16,
+    ) -> Self {
+        SocketAddr::V6(SocketAddrV6::new_ip6_port(
+            a0, a1, a2, a3, a4, a5, a6, a7, port,
+        ))
     }
 
     pub fn len(&self) -> usize {
@@ -155,7 +165,7 @@ mod ipv4 {
 
     use smoltcp::wire::Ipv4Address;
 
-    use super::{Error, port_to_bytes, Result};
+    use super::{port_to_bytes, Error, Result};
 
     pub fn ipv4_addr_from_bytes(bytes: &[u8]) -> Result<Ipv4Address> {
         if bytes.len() == 4 {
@@ -181,7 +191,10 @@ mod ipv4 {
         }
 
         pub fn new_ip4_port(a0: u8, a1: u8, a2: u8, a3: u8, port: u16) -> Self {
-            SocketAddrV4 { addr: Ipv4Address::new(a0, a1, a2, a3), port }
+            SocketAddrV4 {
+                addr: Ipv4Address::new(a0, a1, a2, a3),
+                port,
+            }
         }
 
         pub fn len(&self) -> usize {
@@ -216,7 +229,7 @@ mod ipv6 {
     use byteorder::{BigEndian, ByteOrder};
     use smoltcp::wire::Ipv6Address;
 
-    use super::{Error, port_to_bytes, Result};
+    use super::{port_to_bytes, Error, Result};
 
     #[cfg(all(feature = "proto-ipv6"))]
     pub fn ipv6_addr_from_bytes(bytes: &[u8]) -> Result<Ipv6Address> {
@@ -227,8 +240,16 @@ mod ipv6 {
         }
     }
 
-    pub fn ipv6_addr(a0: u16, a1: u16, a2: u16, a3: u16,
-                     a4: u16, a5: u16, a6: u16, a7: u16) -> Ipv6Address {
+    pub fn ipv6_addr(
+        a0: u16,
+        a1: u16,
+        a2: u16,
+        a3: u16,
+        a4: u16,
+        a5: u16,
+        a6: u16,
+        a7: u16,
+    ) -> Ipv6Address {
         let ip_address: [u16; 8] = [a0, a1, a2, a3, a4, a5, a6, a7];
         let mut ip_address_bytes = [0u8; 16];
         {
@@ -252,10 +273,21 @@ mod ipv6 {
             SocketAddrV6 { addr, port }
         }
 
-        pub fn new_ip6_port(a0: u16, a1: u16, a2: u16, a3: u16,
-                            a4: u16, a5: u16, a6: u16, a7: u16,
-                            port: u16) -> Self {
-            SocketAddrV6 { addr: Ipv6Address::new(a0, a1, a2, a3, a4, a5, a6, a7), port }
+        pub fn new_ip6_port(
+            a0: u16,
+            a1: u16,
+            a2: u16,
+            a3: u16,
+            a4: u16,
+            a5: u16,
+            a6: u16,
+            a7: u16,
+            port: u16,
+        ) -> Self {
+            SocketAddrV6 {
+                addr: Ipv6Address::new(a0, a1, a2, a3, a4, a5, a6, a7),
+                port,
+            }
         }
 
         pub fn len(&self) -> usize {
@@ -292,20 +324,20 @@ mod std {
     #[cfg(feature = "proto-ipv6")]
     use ::std::net::{Ipv6Addr, SocketAddrV6 as StdSocketAddrV6};
 
-    #[cfg(all(feature = "proto-ipv4", feature = "proto-ipv6"))]
-    use smoltcp::wire::{IpAddress, IpEndpoint};
     #[cfg(all(feature = "proto-ipv4"))]
     use smoltcp::wire::Ipv4Address;
     #[cfg(all(feature = "proto-ipv6"))]
     use smoltcp::wire::Ipv6Address;
+    #[cfg(all(feature = "proto-ipv4", feature = "proto-ipv6"))]
+    use smoltcp::wire::{IpAddress, IpEndpoint};
 
-    use super::{Error, Result};
     #[cfg(all(feature = "proto-ipv4", feature = "proto-ipv6"))]
     use super::SocketAddr;
     #[cfg(all(feature = "proto-ipv4"))]
     use super::SocketAddrV4;
     #[cfg(all(feature = "proto-ipv6"))]
     use super::SocketAddrV6;
+    use super::{Error, Result};
 
     #[cfg(feature = "proto-ipv4")]
     pub trait IntoStdIpv4Addr: Sized {
@@ -365,8 +397,12 @@ mod std {
     impl From<SocketAddr> for StdSocketAddr {
         fn from(val: SocketAddr) -> Self {
             match val {
-                SocketAddr::V4(val) => StdSocketAddr::V4(StdSocketAddrV4::new(val.addr.0.into(), val.port)),
-                SocketAddr::V6(val) => StdSocketAddr::V6(StdSocketAddrV6::new(val.addr.0.into(), val.port, 0, 0)),
+                SocketAddr::V4(val) => {
+                    StdSocketAddr::V4(StdSocketAddrV4::new(val.addr.0.into(), val.port))
+                }
+                SocketAddr::V6(val) => {
+                    StdSocketAddr::V6(StdSocketAddrV6::new(val.addr.0.into(), val.port, 0, 0))
+                }
             }
         }
     }
@@ -375,8 +411,12 @@ mod std {
     impl From<&SocketAddr> for StdSocketAddr {
         fn from(val: &SocketAddr) -> Self {
             match val {
-                SocketAddr::V4(val) => StdSocketAddr::V4(StdSocketAddrV4::new(val.addr.0.into(), val.port)),
-                SocketAddr::V6(val) => StdSocketAddr::V6(StdSocketAddrV6::new(val.addr.0.into(), val.port, 0, 0)),
+                SocketAddr::V4(val) => {
+                    StdSocketAddr::V4(StdSocketAddrV4::new(val.addr.0.into(), val.port))
+                }
+                SocketAddr::V6(val) => {
+                    StdSocketAddr::V6(StdSocketAddrV6::new(val.addr.0.into(), val.port, 0, 0))
+                }
             }
         }
     }
@@ -385,8 +425,14 @@ mod std {
     impl From<StdSocketAddr> for SocketAddr {
         fn from(val: StdSocketAddr) -> Self {
             match val {
-                StdSocketAddr::V4(val) => SocketAddr::V4(SocketAddrV4 { addr: Ipv4Address::from(*val.ip()), port: val.port() }),
-                StdSocketAddr::V6(val) => SocketAddr::V6(SocketAddrV6 { addr: Ipv6Address::from(*val.ip()), port: val.port() }),
+                StdSocketAddr::V4(val) => SocketAddr::V4(SocketAddrV4 {
+                    addr: Ipv4Address::from(*val.ip()),
+                    port: val.port(),
+                }),
+                StdSocketAddr::V6(val) => SocketAddr::V6(SocketAddrV6 {
+                    addr: Ipv6Address::from(*val.ip()),
+                    port: val.port(),
+                }),
             }
         }
     }
@@ -409,7 +455,9 @@ mod tests {
     #[cfg(all(feature = "proto-ipv4"))]
     #[test]
     fn ipv4_socket_addr() {
-        if env::var("RUST_LOG").is_err() { env::set_var("RUST_LOG", "debug"); }
+        if env::var("RUST_LOG").is_err() {
+            env::set_var("RUST_LOG", "debug");
+        }
         let _ = pretty_env_logger::try_init_timed();
 
         let ip_address_bytes = [127, 0, 0, 1];
@@ -423,14 +471,19 @@ mod tests {
         info!("socket_addr debug: {:?}", socket_addr);
         assert_eq!(socket_addr.len(), 6);
         assert_eq!(socket_addr.addr.as_bytes(), &ip_address_bytes);
-        assert_eq!(socket_addr.addr.as_bytes(), &socket_addr.to_vec().as_slice()[0..4]);
+        assert_eq!(
+            socket_addr.addr.as_bytes(),
+            &socket_addr.to_vec().as_slice()[0..4]
+        );
         assert_eq!(socket_addr.port, 8080);
     }
 
     #[cfg(all(feature = "proto-ipv6"))]
     #[test]
     fn ipv6_socket_addr() {
-        if env::var("RUST_LOG").is_err() { env::set_var("RUST_LOG", "debug"); }
+        if env::var("RUST_LOG").is_err() {
+            env::set_var("RUST_LOG", "debug");
+        }
         let _ = pretty_env_logger::try_init_timed();
 
         let ip_address_bytes: [u8; 16] = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1];
@@ -446,14 +499,19 @@ mod tests {
         info!("socket_addr debug: {:?}", socket_addr);
         assert_eq!(socket_addr.len(), 18);
         assert_eq!(socket_addr.addr.as_bytes(), &ip_address_bytes);
-        assert_eq!(socket_addr.addr.as_bytes(), &socket_addr.to_vec().as_slice()[0..16]);
+        assert_eq!(
+            socket_addr.addr.as_bytes(),
+            &socket_addr.to_vec().as_slice()[0..16]
+        );
         assert_eq!(socket_addr.port, 8080);
     }
 
     #[cfg(all(feature = "std", feature = "proto-ipv4", feature = "proto-ipv6"))]
     #[test]
     fn ipv4_to_std() {
-        if env::var("RUST_LOG").is_err() { env::set_var("RUST_LOG", "debug"); }
+        if env::var("RUST_LOG").is_err() {
+            env::set_var("RUST_LOG", "debug");
+        }
         let _ = pretty_env_logger::try_init_timed();
 
         let ip_address_bytes = [127, 0, 0, 1];
@@ -466,8 +524,14 @@ mod tests {
         let socket_addr_v4 = SocketAddrV4::new(ip_address, 80);
         assert_eq!(socket_addr_v4.len(), 6);
         let socket_addr = SocketAddr::V4(socket_addr_v4);
-        assert_eq!(Ok(socket_addr), SocketAddr::new(IpAddress::Ipv4(ip_address), 80));
-        assert_eq!(Err(Error::UnspecifiedIp), SocketAddr::new(IpAddress::Unspecified, 80));
+        assert_eq!(
+            Ok(socket_addr),
+            SocketAddr::new(IpAddress::Ipv4(ip_address), 80)
+        );
+        assert_eq!(
+            Err(Error::UnspecifiedIp),
+            SocketAddr::new(IpAddress::Unspecified, 80)
+        );
         assert_eq!(socket_addr, SocketAddr::new_v4(ip_address, 80));
         assert_eq!(socket_addr, SocketAddr::new_ip4_port(127, 0, 0, 1, 80));
         assert_eq!(socket_addr.len(), 6);
@@ -489,7 +553,9 @@ mod tests {
     #[cfg(all(feature = "std", feature = "proto-ipv4", feature = "proto-ipv6"))]
     #[test]
     fn ipv6_to_std() {
-        if env::var("RUST_LOG").is_err() { env::set_var("RUST_LOG", "debug"); }
+        if env::var("RUST_LOG").is_err() {
+            env::set_var("RUST_LOG", "debug");
+        }
         let _ = pretty_env_logger::try_init_timed();
 
         let ip_address_bytes: [u8; 16] = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1];
@@ -497,21 +563,36 @@ mod tests {
         let std_ip_address: Ipv6Addr = ip_address.into_std();
         assert_eq!(std_ip_address.octets().as_ref(), ip_address_bytes);
         let std_ip_address: Result<IpAddr> = IpAddress::Ipv6(ip_address).into_std();
-        assert_eq!(std_ip_address, Ok(IpAddr::V6(Ipv6Addr::new(0, 0, 0, 0, 0, 0, 0, 1))));
+        assert_eq!(
+            std_ip_address,
+            Ok(IpAddr::V6(Ipv6Addr::new(0, 0, 0, 0, 0, 0, 0, 1)))
+        );
 
         let socket_addr_v6 = SocketAddrV6::new(ip_address, 80);
         assert_eq!(socket_addr_v6.len(), 18);
         let socket_addr = SocketAddr::V6(socket_addr_v6);
-        assert_eq!(Ok(socket_addr), SocketAddr::new(IpAddress::Ipv6(ip_address), 80));
-        assert_eq!(Err(Error::UnspecifiedIp), SocketAddr::new(IpAddress::Unspecified, 80));
+        assert_eq!(
+            Ok(socket_addr),
+            SocketAddr::new(IpAddress::Ipv6(ip_address), 80)
+        );
+        assert_eq!(
+            Err(Error::UnspecifiedIp),
+            SocketAddr::new(IpAddress::Unspecified, 80)
+        );
         assert_eq!(socket_addr, SocketAddr::new_v6(ip_address, 80));
-        assert_eq!(socket_addr, SocketAddr::new_ip6_port(0, 0, 0, 0, 0, 0, 0, 1, 80));
+        assert_eq!(
+            socket_addr,
+            SocketAddr::new_ip6_port(0, 0, 0, 0, 0, 0, 0, 1, 80)
+        );
         assert_eq!(socket_addr.len(), 18);
         assert_eq!(socket_addr.ip().as_bytes(), ip_address_bytes);
         assert_eq!(socket_addr.ip_octets(), ip_address_bytes);
         let socket_addr_vec = socket_addr.to_vec();
         assert_eq!(socket_addr_vec[0..16], ip_address_bytes);
-        assert_eq!(port_from_bytes(socket_addr_vec[16], socket_addr_vec[17]), 80);
+        assert_eq!(
+            port_from_bytes(socket_addr_vec[16], socket_addr_vec[17]),
+            80
+        );
         info!("socket_addr: {}", socket_addr);
         info!("socket_addr debug: {:?}", socket_addr);
 
